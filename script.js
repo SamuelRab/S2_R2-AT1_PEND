098c71289191f29e67862b5107cc0913fe2c4b7a
SAMUEL RABELO DA SILVA <samuel.silva300@senaisp.edu.br>
2026-03-12 09:43:23 -0300

    Exercicio6

--- script.js
+++ script.js
@@ -16,24 +16,43 @@ mensagem.className = "text-danger fw-bold"
 } else {
 
 let novaTarefa = document.createElement("li")
-
 novaTarefa.className = "list-group-item d-flex justify-content-between align-items-center"
 
-novaTarefa.innerText = tarefa
+let checkbox = document.createElement("input")
+checkbox.type = "checkbox"
+checkbox.className = "form-check-input me-2"
+
+let textoTarefa = document.createElement("span")
+textoTarefa.innerText = tarefa
 
 let btnRemover = document.createElement("button")
-
 btnRemover.innerText = "Remover"
 btnRemover.className = "btn btn-danger btn-sm"
 
-btnRemover.addEventListener("click", () => {
+checkbox.addEventListener("change", () => {
 
-novaTarefa.remove()
+if(checkbox.checked){
+textoTarefa.style.textDecoration = "line-through"
+textoTarefa.style.color = "gray"
+}else{
+textoTarefa.style.textDecoration = "none"
+textoTarefa.style.color = "black"
+}
 
 })
+
+btnRemover.addEventListener("click", () => {
+novaTarefa.remove()
+})
+
+novaTarefa.appendChild(checkbox)
+novaTarefa.appendChild(textoTarefa)
 novaTarefa.appendChild(btnRemover)
+
 listaTarefas.appendChild(novaTarefa)
+
 inputTarefa.value = ""
+
 mensagem.innerText = "Tarefa adicionada com sucesso!"
 mensagem.className = "text-success fw-bold"
 
